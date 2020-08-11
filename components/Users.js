ceeeb5fe6d607e99b62538596f244fd3d18d9cab
safi <safiazim8@gmail.com>
2020-08-11 23:43:22 +0500

    Styling with style-jsx

--- components/Users.js
+++ components/Users.js
@@ -2,10 +2,20 @@ import React from "react";
 
 const users = (props) => {
   return (
-    <duv>
+    <div>
       <h1>{props.name}</h1>
       <p>Age:{props.age}</p>
-    </duv>
+      <style jsx>
+        {`
+          div {
+            border: 1px solid #eee;
+            box-shadow: 0 2px 3px #ccc;
+            padding: 20px;
+            text-align: center;
+          }
+        `}
+      </style>
+    </div>
   );
 };
 
