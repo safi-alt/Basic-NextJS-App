7381dd78d46e4887cae14e39d4152fe1c0f77f85
safi <safiazim8@gmail.com>
2020-08-11 22:55:07 +0500

    Creating basic NextJS App

---
+++ pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const indexPage = () => {
+  return (
+    <div>
+      <h1>This is the INDEX PAGE</h1>
+    </div>
+  );
+};
+
+export default indexPage;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const indexPage = () => {
+  return (
+    <div>
+      <h1>This is the main page</h1>
+    </div>
+  );
+};
+
+export default indexPage;
