abbef1922c9266f63337bcaa69f07ef833e2a5fa
safi <safiazim8@gmail.com>
2020-08-11 23:35:00 +0500

    Components and pages routing in nextJS

---
+++ components/Users.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const users = (props) => {
+  return (
+    <duv>
+      <h1>{props.name}</h1>
+      <p>Age:{props.age}</p>
+    </duv>
+  );
+};
+
+export default users;
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Users from "../../components/Users";
 
 const indexPage = () => {
   return (
     <div>
       <h1>This is the INDEX PAGE</h1>
+      <Users name="Safi" age={21} />
     </div>
   );
 };
